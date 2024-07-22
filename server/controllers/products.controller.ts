46035887e6dbef5a5e2637b24e8ee507fd528d83
Lautaro Galvan <lauty.galvan.96@gmail.com>
2024-07-22 16:42:36 -0300

    Implement CRUD operations for products

--- server/controllers/products.controller.ts
+++ server/controllers/products.controller.ts
@@ -7,9 +7,31 @@ class ProductsController {
 
     static GetAllProductsAsync = async (req: Request, res: Response) => {
         const products = await ProductsService.GetProductsAsync();
+
+        if (!products) {
+            return ObjectResult.SendNotFound(res, "Products not found");
+        }
+
         return ObjectResult.SendOk(res, products);
     };
 
+    static GetProductByIdAsync = async (req: Request, res: Response) => {
+        const id = req.params.id;
+        const product = await ProductsService.GetProductByIdAsync(Number(id));
+
+        if (!product) return ObjectResult.SendNotFound(res, "Product not found");
+
+        return ObjectResult.SendOk(res, product);
+    };
+
+    static DeleteProductAsync = async (req: Request, res: Response) => {
+        const id = req.params.id;
+        const product = await ProductsService.DeleteProductAsync(Number(id));
+
+        if (!product) return ObjectResult.SendNotFound(res, "Product not found");
+
+        return ObjectResult.SendOk(res, "Se ha eliminado el producto correctamente");
+    };
 }
 
 export default ProductsController;
--- server/services/products.services.ts
+++ server/services/products.services.ts
@@ -5,6 +5,19 @@ class ProductsService {
         return products;
     };
 
+    static GetProductByIdAsync = async (id: number) => {
+        const products = await Product.GetProductsAsync();
+
+        const product = products.find(x => x.Id == id);
+
+        return product;
+    };
+
+    static DeleteProductAsync = async (id: number) => {
+        const deletedProduct = await Product.DeleteProductAsync(id);
+        return deletedProduct;
+    };
+
 }
 
 export default ProductsService;
